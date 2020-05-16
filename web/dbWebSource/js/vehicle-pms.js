566218c6f87b718dff79e4fa37644c58aa50eac0
MJDT <markjoseph.tadlip@gmail.com>
2020-05-16 15:04:09 +0800

    Add replacement parts

--- web/dbWebSource/js/vehicle-pms.js
+++ web/dbWebSource/js/vehicle-pms.js
@@ -1,12 +1,11 @@
   var pms = (function(){
-    var  _public            = {}
-        ,bs                 = zsi.bs.ctrl
-        ,svn                = zsi.setValIfNull 
+    var  _pub = {}
+        ,gPMSid  = ""
     ;
     zsi.ready = function(){
         $(".page-title").html("Vehicle PMS");
         $(".panel-container").css("min-height", $(window).height() - 160);
-        validations();
+        //validations();
         //$("#client_phone_no").inputmask({"mask": "(99) 9999 - 9999"});
         $("#pms_date").datepicker({todayHighlight:true}).datepicker("setDate",new Date());
         $("#pms_type_id").dataBind({
@@ -14,9 +13,8 @@
            ,text         : "pms_desc"
            ,value        : "pms_type_id"
            ,onChange     : function(d){
-               var _info           = d.data[d.index - 1]
-                   pms_type_id     = isUD(_info) ? "" : _info.pms_type_id;
-               
+                var _info           = d.data[d.index - 1]
+                    ,pms_type_id     = isUD(_info) ? "" : _info.pms_type_id;
            }
         });
         $("#vehicle_id").dataBind({
@@ -24,39 +22,108 @@
            ,text         : "plate_no"
            ,value        : "vehicle_id"
            ,onChange     : function(d){
-               var _info           = d.data[d.index - 1]
-                   vehicle_id     = isUD(_info) ? "" : _info.vehicle_id;
-               
+                var _info           = d.data[d.index - 1]
+                    ,vehicle_id     = isUD(_info) ? "" : _info.vehicle_id;
+           }
+        });
+        $("#status_id").dataBind({
+            sqlCode      : "S122" //statuses_sel
+           ,text         : "status_name"
+           ,value        : "status_id"
+           ,onChange     : function(d){
+                var _info           = d.data[d.index - 1]
+                    ,status_id     = isUD(_info) ? "" : _info.status_id; 
            }
         });
         
+        dispalyReplacementParts();
     };
     
-    $("#btnSavePMS").click(function () {
-        $("#formPMS").jsonSubmit({
-             procedure: "vehicle_pms_upd"
-            ,isSingleEntry: true
-            ,onComplete: function (data) {
-                if(data.isSuccess){
-                   if(data.isSuccess===true) zsi.form.showAlert("alert");
-                   $("form").removeClass('was-validated');
-                   $("#formPMS").find("input").val("");
-                   $("#formPMS").find("textarea").val("");
-                   $("#formPMS").find("select").val(null).trigger('change');
-                   $("#myModal").find("#msg").text("Data successfully saved.");
-                   $("#myModal").find("#msg").css("color","green");
-                   setTimeout(function(){
-                       $("#myModal").modal('toggle');
-                       $("#pms_date").datepicker({todayHighlight:true}).datepicker("setDate",new Date());
-                       modalTxt();
-                   },1000);
-                }else{
-                   $("#myModal").find("#msg").text("Something went wrong when saving the data.");
-                   $("#myModal").find("#msg").css("color","red");
+    function dispalyReplacementParts(){
+        var _dataBind = {
+            height : $("#divReplacementParts").closest(".panel-container").height()
+            ,blankRowsLimit : 10
+            ,dataRows : [
+                {text: "Item No.", width: 60
+                    ,onRender : function(d){ 
+                        return app.bs({type: "hidden", name: "replacement_id", value: app.svn(d,"replacement_id")})
+                            +  app.bs({type: "hidden", name: "is_edited"})  
+                            +  app.bs({type: "hidden", name: "pms_id", value: gPMSid})  
+                            +  app.bs({type: "hidden", name: "repair_id"})  
+                            +  app.bs({type: "input",  name: "seq_no", value: app.svn(d,"seq_no"), style: "text-align:center"}); 
+                    }
                 }
+                ,{text: "Part", type: "select", name: "part_id", width: 130, style: "text-align:left"}
+                ,{text: "Part Qty", width: 120
+                    ,onRender : function(d){ 
+                        return app.bs({type: "input",  name: "part_qty", value: commaSeparateNumber(app.svn(d,"part_qty")), style: "text-align:center"});
+                    }
+                }
+                ,{text: "Unit", type: "select", name: "unit_id", width: 130, style: "text-align:left"}
+                ,{text: "Unit Cost", width: 120
+                    ,onRender : function(d){ 
+                        return app.bs({type: "input",  name: "unit_cost", value: commaSeparateNumber(app.svn(d,"unit_cost")), style: "text-align:center"});
+                    }
+                }
+                ,{text: "Total Cost", width: 120
+                    ,onRender : function(d){ 
+                        return app.bs({type: "input",  name: "total_cost", value: commaSeparateNumber(app.svn(d,"total_cost")), style: "text-align:center"});
+                    }
+                }
+                ,{text: "Replaced (Yes/No)", type: "yesno", name: "is_replacement", width: 120, style: "text-align:center", defaultValue:"N"}
+                ,{text: "New (Yes/No)", type: "yesno", name: "is_bnew", width: 100, style: "text-align:center", defaultValue:"N"}
+            ]
+            ,onComplete : function(o){
+                var _$this = this;
+                _$this.find("[name='part_id']").dataBind({
+                    sqlCode      : "D256" //dd_parts_sel
+                   ,text         : "part_desc"
+                   ,value        : "part_id"
+                   ,onChange     : function(d){
+                    //   var _info = d.data[d.index - 1];
+                    //   part_id = isUD(_info) ? "" : _info.part_id; 
+                   }
+                });
+                _$this.find("[name='unit_id']").dataBind({
+                    sqlCode      : "D257" //dd_units_sel
+                   ,text         : "unit_name"
+                   ,value        : "unit_id"
+                   ,onChange     : function(d){
+                    //   var _info = d.data[d.index - 1];
+                    //   unit_id = isUD(_info) ? "" : _info.unit_id; 
+                   }
+                });
+                
+                _$this.find("[name='part_qty'],[name='unit_cost']").focusout(function(){
+                    var _$row = $(this).closest(".zRow");
+                    var _$totalCost = _$row.find("[name='total_cost']")
+                        ,_$partQty = _$row.find("[name='part_qty']")
+                        ,_$unitCost = _$row.find("[name='unit_cost']")
+                        ,_partQty = _$partQty.val().replace(/,/g, "")
+                        ,_unitCost = _$unitCost.val().replace(/,/g, "")
+                        ,_totalCost = "";
+                        
+                        if(_partQty!=="" && _unitCost!==""){
+                            _totalCost = parseFloat(_partQty).toFixed(2) * parseFloat(_unitCost).toFixed(2);
+                            _$totalCost.val(commaSeparateNumber(_totalCost));
+                        }else{
+                            _$totalCost.val("");
+                        }
+                });
+                
+                _$this.find("[name='total_cost']").attr("disabled",true);
+                _$this.find("[name='part_qty'],[name='unit_cost']").addClass("numeric");
+                zsi.initInputTypesAndFormats();
             }
-        }); 
-    });
+        };
+        
+        if(gPMSid!==""){
+            _dataBind.sqlCode = "P249"; //part_replacements_sel
+            _dataBind.parameters = {pms_id: gPMSid};
+        }
+        
+        $("#gridReplacementParts").dataBind(_dataBind);
+    }
     
     function modalTxt(){
         setTimeout(function(){
@@ -65,29 +132,107 @@
         },1000);
     }
     
+    function commaSeparateNumber(n){
+        var _res = "";
+        if($.isNumeric(n)){
+            var _num = parseFloat(n).toFixed(2).toString().split(".");
+            _res = _num[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (!isUD(_num[1]) ? "." + _num[1] : "");
+        }
+        return _res;
+    }
+    
     function validations(){
         var forms = document.getElementsByClassName('needs-validation');
     	// Loop over them and prevent submission
     	var validation = Array.prototype.filter.call(forms, function(form) {
-		form.addEventListener('submit', function(event) {
-			if (form.checkValidity() === false) {
-				event.preventDefault();
-				event.stopPropagation();
-			    $("form").addClass('was-validated');
-			}else{
-    			event.preventDefault();
-    			event.stopPropagation();
-			    $('#myModal').modal('show');
-			    $("form").addClass('was-validated');
-			}
-		}, false);
-	});
-  }
-
-   
+    		form.addEventListener('submit', function(event) {
+    			if (form.checkValidity() === false) {
+    				event.preventDefault();
+    				event.stopPropagation();
+    			    $("form").addClass('was-validated');
+    			}else{
+        			event.preventDefault();
+        			event.stopPropagation();
+    			    $('#myModal').modal('show');
+    			    $("form").addClass('was-validated');
+    			}
+    		}, false);
+    	});
+    }
     
-    return _public;
+    function submitReplacementParts(){
+        var _$frm = $("#formPMS")
+            ,_$grid = $("#gridReplacementParts");
+        
+        _$frm.find("[name='pms_id']").val(gPMSid);
+        _$grid.find("[name='part_qty'], [name='unit_cost']").each(function(){
+            this.value = this.value.replace(/,/g, "");
+        });
+        _$grid.jsonSubmit({
+             procedure: "part_replacements_upd" 
+             ,notIncludes: ["total_cost"]
+             ,onComplete: function(data){
+                if(data.isSuccess){
+                    zsi.form.showAlert("alert"); 
+                    
+                    $("#btnNew").removeClass("hide");
+                    dispalyReplacementParts();
+                }
+             }
+        });
+    }
     
+    $("#btnNew").click(function () {
+        var _params = ['#p','vehicle-pms'].join("/");
+        window.open(_params,"_self");    
+    });
     
+    $("#btnSave").click(function () {
+        var _$div = $("#divVehiclePMS") 
+            ,_date = $("#pms_date").val()
+            ,_pmsType = $("#pms_type_id").val()
+            ,_vehicle = $("#vehicle_id").val()
+            ,_odoReading = $("#odo_reading").val()
+            ,_amount = $("#pm_amount").val().replace(/,/g, "")
+            ,_location = $("#pm_location").val()
+            ,_comment = $("#comment").val()
+            ,_status = $("#status_id").val();
+        
+        $.post(app.procURL + "vehicle_pms_upd @pms_id="+ gPMSid +",@pms_date='"+ _date +"',@pms_type_id="+ _pmsType +",@vehicle_id="+ _vehicle +",@odo_reading="+ _odoReading
+                            +",@pm_amount="+ _amount +",@pm_location='" +_location +"',@comment='"+ _comment +"',@status_id="+ _status
+            ,function(data){
+                if(data.isSuccess){
+                    gPMSid = data.returnValue;
+                    submitReplacementParts();
+                }      
+        });
+    });     
     
-})();            
+    // $("#btnSavePMS").click(function () {
+    //     $("#formPMS").jsonSubmit({
+    //          procedure: "vehicle_pms_upd"
+    //         ,isSingleEntry: true
+    //         ,onComplete: function (data) {
+    //             if(data.isSuccess){
+    //               if(data.isSuccess===true) zsi.form.showAlert("alert");
+    //               $("form").removeClass('was-validated');
+    //               $("#formPMS").find("input").val("");
+    //               $("#formPMS").find("textarea").val("");
+    //               $("#formPMS").find("select").val(null).trigger('change');
+    //               $("#myModal").find("#msg").text("Data successfully saved.");
+    //               $("#myModal").find("#msg").css("color","green");
+    //               setTimeout(function(){
+    //                   $("#myModal").modal('toggle');
+    //                   $("#pms_date").datepicker({todayHighlight:true}).datepicker("setDate",new Date());
+    //                   modalTxt();
+    //               },1000);
+    //             }else{
+    //               $("#myModal").find("#msg").text("Something went wrong when saving the data.");
+    //               $("#myModal").find("#msg").css("color","red");
+    //             }
+    //         }
+    //     }); 
+    // });
+    
+    return _pub;
+})();              
--- web/dbWebSource/js/vehicleRepair.js
+++ web/dbWebSource/js/vehicleRepair.js
@@ -1,15 +1,110 @@
  
   var vehicleRepair = (function(){
-    var  _public            = {}
-        ,bs                 = zsi.bs.ctrl
-        ,svn                = zsi.setValIfNull 
+    var  _public = {}
+        ,gRepairId = "" 
     ;
     zsi.ready = function(){
         $(".page-title").html("Vehicle Repair");
         $(".panel-container").css("min-height", $(window).height() - 160);   
-        validations();
+        //validations();
+        dispalyReplacementParts();
         selects();
     };
+    function dispalyReplacementParts(){
+        var _dataBind = {
+            height : $("#divReplacementParts").closest(".panel-container").height()
+            ,blankRowsLimit : 10
+            ,dataRows : [
+                {text: "Item No.", width: 60
+                    ,onRender : function(d){ 
+                        return app.bs({type: "hidden", name: "replacement_id", value: app.svn(d,"replacement_id")})
+                            +  app.bs({type: "hidden", name: "is_edited"})  
+                            +  app.bs({type: "hidden", name: "pms_id"})  
+                            +  app.bs({type: "hidden", name: "repair_id", value: gRepairId})  
+                            +  app.bs({type: "input",  name: "seq_no", value: app.svn(d,"seq_no"), style: "text-align:center"}); 
+                    }
+                }
+                ,{text: "Part", type: "select", name: "part_id", width: 130, style: "text-align:left"}
+                ,{text: "Part Qty", width: 120
+                    ,onRender : function(d){ 
+                        return app.bs({type: "input",  name: "part_qty", value: commaSeparateNumber(app.svn(d,"part_qty")), style: "text-align:center"});
+                    }
+                }
+                ,{text: "Unit", type: "select", name: "unit_id", width: 130, style: "text-align:left"}
+                ,{text: "Unit Cost", width: 120
+                    ,onRender : function(d){ 
+                        return app.bs({type: "input",  name: "unit_cost", value: commaSeparateNumber(app.svn(d,"unit_cost")), style: "text-align:center"});
+                    }
+                }
+                ,{text: "Total Cost", width: 120
+                    ,onRender : function(d){ 
+                        return app.bs({type: "input",  name: "total_cost", value: commaSeparateNumber(app.svn(d,"total_cost")), style: "text-align:center"});
+                    }
+                }
+                ,{text: "Replaced (Yes/No)", type: "yesno", name: "is_replacement", width: 120, style: "text-align:center", defaultValue:"N"}
+                ,{text: "New (Yes/No)", type: "yesno", name: "is_bnew", width: 100, style: "text-align:center", defaultValue:"N"}
+            ]
+            ,onComplete : function(o){
+                var _$this = this;
+                _$this.find("[name='part_id']").dataBind({
+                    sqlCode      : "D256" //dd_parts_sel
+                   ,text         : "part_desc"
+                   ,value        : "part_id"
+                   ,onChange     : function(d){
+                    //   var _info = d.data[d.index - 1];
+                    //   part_id = isUD(_info) ? "" : _info.part_id; 
+                   }
+                });
+                _$this.find("[name='unit_id']").dataBind({
+                    sqlCode      : "D257" //dd_units_sel
+                   ,text         : "unit_name"
+                   ,value        : "unit_id"
+                   ,onChange     : function(d){
+                    //   var _info = d.data[d.index - 1];
+                    //   unit_id = isUD(_info) ? "" : _info.unit_id; 
+                   }
+                });
+                
+                _$this.find("[name='part_qty'],[name='unit_cost']").focusout(function(){
+                    var _$row = $(this).closest(".zRow");
+                    var _$totalCost = _$row.find("[name='total_cost']")
+                        ,_$partQty = _$row.find("[name='part_qty']")
+                        ,_$unitCost = _$row.find("[name='unit_cost']")
+                        ,_partQty = _$partQty.val().replace(/,/g, "")
+                        ,_unitCost = _$unitCost.val().replace(/,/g, "")
+                        ,_totalCost = "";
+                        
+                        if(_partQty!=="" && _unitCost!==""){
+                            _totalCost = parseFloat(_partQty).toFixed(2) * parseFloat(_unitCost).toFixed(2);
+                            _$totalCost.val(commaSeparateNumber(_totalCost));
+                        }else{
+                            _$totalCost.val("");
+                        }
+                });
+                
+                _$this.find("[name='total_cost']").attr("disabled",true);
+                _$this.find("[name='part_qty'],[name='unit_cost']").addClass("numeric");
+                zsi.initInputTypesAndFormats();
+            }
+        };
+        
+        if(gRepairId!==""){
+            _dataBind.sqlCode = "P249"; //part_replacements_sel
+            _dataBind.parameters = {repair_id: gRepairId};
+        }
+        
+        $("#gridReplacementParts").dataBind(_dataBind);
+    }
+    
+    function commaSeparateNumber(n){
+        var _res = "";
+        if($.isNumeric(n)){
+            var _num = parseFloat(n).toFixed(2).toString().split(".");
+            _res = _num[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (!isUD(_num[1]) ? "." + _num[1] : "");
+        }
+        return _res;
+    }
+    
     function selects(){
         $("#repair_date").datepicker({todayHighlight:true}).datepicker("setDate",new Date());
         $("#pms_type_id").dataBind({
@@ -30,7 +125,17 @@
                 vehicle_id     = isUD(_info) ? "" : _info.vehicle_id; 
            }
         });
+        $("#status_id").dataBind({
+            sqlCode      : "S122" //statuses_sel
+           ,text         : "status_name"
+           ,value        : "status_id"
+           ,onChange     : function(d){
+               var _info           = d.data[d.index - 1];
+                status_id     = isUD(_info) ? "" : _info.status_id; 
+           }
+        });
     }
+    
     function validations(){ 
         var forms = document.getElementsByClassName('needs-validation'); 
     	var validation = Array.prototype.filter.call(forms, function(form) {
@@ -48,31 +153,99 @@
     			}
     		}, false);
     	});
+    	
     }
-    $("#btnSaveClient").click(function () {
-        var _$frm = $("#formVehicleRepair"); 
-        $("#formVehicleRepair").jsonSubmit({
-             procedure: "vehicle_repairs_upd" 
-            ,isSingleEntry: true
-            ,onComplete: function (data) {
+    // FOR ODO READING INPUT TYPE.
+    function setInputFilter(textbox, inputFilter) {
+      ["input", "keydown"].forEach(function(event) {
+        textbox.addEventListener(event, function() {
+          if (inputFilter(this.value)) {
+            this.oldValue = this.value;
+            this.oldSelectionStart = this.selectionStart;
+            this.oldSelectionEnd = this.selectionEnd;
+          } else if (this.hasOwnProperty("oldValue")) {
+            this.value = this.oldValue;
+            this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+          } else {
+            this.value = "";
+          }
+        });
+      });
+    } 
+    setInputFilter(document.getElementById("odo_reading"), function(value) {
+      return /^-?\d*$/.test(value);  
+    }); 
+    
+    function submitReplacementParts(){
+        var _$frm = $("#formVehicleRepair")
+            ,_$grid = $("#gridReplacementParts");
+        
+        _$frm.find("[name='repair_id']").val(gRepairId);
+        _$grid.find("[name='part_qty'], [name='unit_cost']").each(function(){
+            this.value = this.value.replace(/,/g, "");
+        });
+        _$grid.jsonSubmit({
+             procedure: "part_replacements_upd" 
+             ,notIncludes: ["total_cost"]
+             ,onComplete: function(data){
                 if(data.isSuccess){
-                   if(data.isSuccess===true) zsi.form.showAlert("alert"); 
-                   _$frm.find("input").val("");
-                   _$frm.find("textarea").val(""); 
-                   _$frm.find("select").val(null).trigger('change');
-                   $("form").removeClass('was-validated');
-                   $("#myModal").find("#msg").text("Data successfully saved.");
-                   $("#myModal").find("#msg").css("color","green"); 
-                   setTimeout(function(){
-                       $("#myModal").modal('toggle');
-                   },1000);
-                }else{
-                   $("#myModal").find("#msg").text("Something went wrong when saving the data.");
-                   $("#myModal").find("#msg").css("color","red");
+                    zsi.form.showAlert("alert"); 
+                    
+                    $("#btnNew").removeClass("hide");
+                    dispalyReplacementParts();
                 }
-            }
-        }); 
+             }
+        });
+    }
+      
+    $("#btnNew").click(function () {
+        var _params = ['#p','vehicleRepair'].join("/");
+        window.open(_params,"_self");    
     });
+    
+    $("#btnSave").click(function () {
+        var _$div = $("#divVehicleRepair") 
+            ,_date = $("#repair_date").val()
+            ,_pmsType = $("#pms_type_id").val()
+            ,_vehicle = $("#vehicle_id").val()
+            ,_odoReading = $("#odo_reading").val()
+            ,_amount = $("#repair_amount").val().replace(/,/g, "")
+            ,_location = $("#repair_location").val()
+            ,_comment = $("#comment").val()
+            ,_status = $("#status_id").val();
+            
+        $.post(app.procURL + "vehicle_repairs_upd @repair_id="+ gRepairId +",@repair_date='"+ _date +"',@pms_type_id="+ _pmsType +",@vehicle_id="+ _vehicle +",@odo_reading="+ _odoReading
+                            +",@repair_amount="+ _amount +",@repair_location='" +_location +"',@comment='"+ _comment +"',@status_id="+ _status
+            ,function(data){
+                if(data.isSuccess){
+                    gRepairId = data.returnValue;
+                    submitReplacementParts();
+                }      
+        });
+    });                
+    
+    // $("#btnSave").click(function () {
+        // _$div.jsonSubmit({
+        //      procedure: "vehicle_repairs_upd" 
+        //     ,parameters: {repair_date: _date, pms_type_id: _pmsType, vehicle_id: _vehicle, odo_reading: _odoReading, repair_amount: _amount, repair_location: _location, comment: _comment, status_id: _status}
+        //     ,isSingleEntry: true
+        //     ,onComplete: function (data) {
+        //         if(data.isSuccess){
+        //             _$div.find("input, textarea").val("");
+                        // _$div.find("select").val(null).trigger('change');
+                        // _$frm.removeClass('was-validated');
+                        // $("#myModal").find("#msg").text("Data successfully saved.");
+                    // $("#myModal").find("#msg").css("color","green"); 
+                    // setTimeout(function(){
+                    //     $("#myModal").modal('toggle');
+                    // },1000);
+        //         }else{
+        //           $("#myModal").find("#msg").text("Something went wrong when saving the data.");
+        //           $("#myModal").find("#msg").css("color","red");
+        //         }
+        //     }
+        // }); 
+    // });
       
     return _public;
     
@@ -88,4 +261,4 @@
 
 
 
- 
+       
